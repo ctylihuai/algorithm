339f222e4eef6c2816ca9f623a2446b0412c2651
laiguangying <274268859@qq.com>
2019-09-19 11:14:34 +0000

    add insert sort

--- sort/insert.c
+++ sort/insert.c
@@ -1,5 +1,20 @@
 #include"pub.h"
 int insert_sort(int arr[], int len)
 {
+	for(int i = 1; i < len; i++)
+	{
+		int tmp = arr[i];
+		int j = 0;
+		for(j = i - 1; j >= 0; j--)
+		{
+			if (tmp < arr[j])
+			{
+				arr[j+1] = arr[j];
+			} else {
+				break;
+			}
+		}
+		arr[j+1] = tmp;
+	}
 	return 0;
 }
